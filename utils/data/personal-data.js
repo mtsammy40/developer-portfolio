fb72f1ceaba5fcc077ffc07608a3cc843af1b620
Samuel Mutemi <mtsammy40@gmail.com>
2025-04-29 14:22:43 +0300

    :sparkles: feat: Fix links.
    Update profile photo

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,8 @@
 export const personalData = {
   name: "Samuel Mutemi Mwangangi",
-  profile: '/profile.jpg',
+  profile: '/profile_2.jpg',
   designation: "Software Engineer",
-  description: "My name is Samuel Mutemi. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "I'm Samuel Mutemi, a Full-Stack Developer with over a decade of turning coffee into clean code. JavaScript is my native language (including its weird dialects - I've made peace with 'undefined' and 'NaN'). I've built everything from lightning-fast React applications to bulletproof Node.js APIs, with battle-tested experience across the entire web stack. My superpower? Taking complex problems and turning them into elegant solutions. When I'm not debating tabs vs spaces or rescuing legacy jQuery codebases, I'm mentoring junior developers (someone has to warn them about dependency hell).I approach every project with three principles: (1) Performance matters, (2) Maintainability isn't optional. Currently available for challenging projects where my mix of technical depth and hard-won experience can make an impact. Let's build something remarkable together - or at least debug it until it works.",
   email: 'iammutemi@gmail.com',
   phone: '+254724049955',
   address: 'Nairobi, Kenya ',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/9914081/samuel-mutemi',
   leetcode: "https://leetcode.com/u/mtsammy40/",
   devUsername: "mtsammy40",
-  resume: "https://1drv.ms/b/s!ApKeGVj_gZAohMwO6JgXLolYZjT26w?e=0WgYuo"
+  resume: "https://1drv.ms/b/c/289081ff58199e92/EYYiMByxVw1KuGDweTjOdsIBVPd3WvYkh8ts6DvisRR9Vw?e=bHtdDM"
 }
